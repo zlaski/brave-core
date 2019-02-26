42b9f40f0d3e38995a87085c74838ddd7203f4d4
petemill <miller.pete+commit@gmail.com>
2019-02-26 15:55:47 -0800

    Improve patch for optimize_web_ui.py

---
+++ browser/resources/brave_optimize_webui_config.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2019 The Brave Authors. All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# you can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+
+def vulcanize_base_args():
+  return [
+    '--exclude', 'chrome://brave-resources/fonts/muli.css',
+    '--exclude', 'chrome://brave-resources/fonts/poppins.css',
+  ]
+
+def url_mappings(src_path):
+  br_resources_path = os.path.join(src_path, 'brave', 'ui', 'webui', 'resources')
+  return [
+    ('chrome://brave-resources/', br_resources_path)
+  ]
