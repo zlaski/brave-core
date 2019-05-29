cbe36c6ce62d86da4ecf230b5adb28d0cc88ed5f
Anthony Tseng <darkdh@gmail.com>
2019-05-28 21:40:24 -0700

    Android build fix
    brave_sync deps fix

--- browser/ui/webui/brave_web_ui_controller_factory.cc
+++ browser/ui/webui/brave_web_ui_controller_factory.cc
@@ -10,11 +10,11 @@
 #include "base/memory/ptr_util.h"
 #include "brave/browser/ui/webui/brave_adblock_ui.h"
 #include "brave/browser/ui/webui/brave_new_tab_ui.h"
-#include "brave/browser/ui/webui/sync/sync_ui.h"
 #include "brave/common/webui_url_constants.h"
 #include "brave/components/brave_rewards/browser/buildflags/buildflags.h"
 #include "brave/components/brave_sync/brave_sync_service.h"
 #include "chrome/common/url_constants.h"
+#include "extensions/buildflags/buildflags.h"
 #include "url/gurl.h"
 
 #if !defined(OS_ANDROID)
@@ -26,6 +26,10 @@
 #include "brave/browser/ui/webui/brave_tip_ui.h"
 #include "brave/browser/ui/webui/brave_rewards_internals_ui.h"
 #include "brave/browser/ui/webui/brave_rewards_ui.h"
+#endif
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+#include "brave/browser/ui/webui/sync/sync_ui.h"
 #endif
 
 using content::WebUI;
@@ -47,10 +51,13 @@ WebUIController* NewWebUI(WebUI* web_ui, const GURL& url) {
 template<>
 WebUIController* NewWebUI<BasicUI>(WebUI* web_ui, const GURL& url) {
   auto host = url.host_piece();
-  if (host == kBraveUISyncHost && brave_sync::BraveSyncService::is_enabled()) {
-    return new SyncUI(web_ui, url.host());
-  } else if (host == kAdblockHost) {
+  if (host == kAdblockHost) {
     return new BraveAdblockUI(web_ui, url.host());
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+  } else if (host == kBraveUISyncHost &&
+             brave_sync::BraveSyncService::is_enabled()) {
+    return new SyncUI(web_ui, url.host());
+#endif
 #if BUILDFLAG(BRAVE_REWARDS_ENABLED)
   } else if (host == kRewardsHost) {
     return new BraveRewardsUI(web_ui, url.host());
