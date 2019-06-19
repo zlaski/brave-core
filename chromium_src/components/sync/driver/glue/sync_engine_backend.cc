0055d803d74dd3e213ca9dad2cf4cf239a531e0c
bridiver <github@brianjohnson.cc>
2019-06-18 17:53:41 -0700

    remove sync engine header patches

--- chromium_src/components/sync/driver/glue/sync_engine_backend.cc
+++ chromium_src/components/sync/driver/glue/sync_engine_backend.cc
@@ -3,43 +3,72 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "../../../../../../components/sync/driver/glue/sync_engine_backend.cc"   // NOLINT
-
-#include "brave/chromium_src/components/sync/driver/glue/sync_engine_backend.h"
-
-#include <memory>
 #include <utility>
 
+#include "base/memory/weak_ptr.h"
 #include "brave/components/brave_sync/jslib_messages.h"
+#include "components/sync/driver/glue/sync_engine_impl.h"
+#include "components/sync/engine/sync_engine_host.h"
+
+using brave_sync::GetRecordsCallback;
+using brave_sync::RecordsList;
 
 namespace syncer {
 
-void SyncEngineBackend::OnNudgeSyncCycle(
-    brave_sync::RecordsListPtr records_list) {
-  host_.Call(FROM_HERE,
-             &SyncEngineImpl::HandleNudgeSyncCycle,
-             base::Passed(&records_list));
+void OnNudgeSyncCycleOnSyncThread(base::WeakPtr<SyncEngineImpl> sync_engine,
+                                  SyncEngineHost* host,
+                                  brave_sync::RecordsListPtr records_list) {
+  if (sync_engine.get())
+    host->OnNudgeSyncCycle(std::move(records_list));
 }
 
-void SyncEngineBackend::OnPollSyncCycle(GetRecordsCallback cb,
-                                          base::WaitableEvent* wevent) {
-  host_.Call(FROM_HERE,
-             &SyncEngineImpl::HandlePollSyncCycle, cb, wevent);
+void OnNudgeSyncCycle(WeakHandle<SyncEngineImpl> sync_engine_impl,
+                      SyncEngineHost* host,
+                      brave_sync::RecordsListPtr records_list) {
+  sync_engine_impl.Call(FROM_HERE,
+                        &OnNudgeSyncCycleOnSyncThread,
+                        host,
+                        base::Passed(&records_list));
 }
 
-void SyncEngineBackend::DoDispatchGetRecordsCallback(
-    GetRecordsCallback cb, std::unique_ptr<RecordsList> records) {
+void OnPollSyncCycleOnSyncThread(base::WeakPtr<SyncEngineImpl> sync_engine,
+                                 SyncEngineHost* host,
+                                 GetRecordsCallback cb,
+                                 base::WaitableEvent* wevent) {
+  if (sync_engine.get())
+    host->OnPollSyncCycle(cb, wevent);
+}
+
+void OnPollSyncCycle(WeakHandle<SyncEngineImpl> sync_engine_impl,
+                     SyncEngineHost* host,
+                     GetRecordsCallback cb,
+                     base::WaitableEvent* wevent) {
+  sync_engine_impl.Call(FROM_HERE,
+                        &OnPollSyncCycleOnSyncThread,
+                        host,
+                        cb,
+                        wevent);
+}
+
+void DoDispatchGetRecordsCallback(GetRecordsCallback cb,
+                                  std::unique_ptr<brave_sync::RecordsList> records) {
   cb.Run(std::move(records));
 }
 
-void SyncEngineBackend::BraveInit(SyncManager::InitArgs* args) {
+void BraveInit(WeakHandle<SyncEngineImpl> sync_engine_impl,
+               SyncEngineHost* host,
+               SyncManager::InitArgs* args) {
   DCHECK(args);
   args->nudge_sync_cycle_delegate_function =
-    base::BindRepeating(&SyncEngineBackend::OnNudgeSyncCycle,
-                        weak_ptr_factory_.GetWeakPtr());
+      base::BindRepeating(&OnNudgeSyncCycle,
+                          sync_engine_impl,
+                          host);
   args->poll_sync_cycle_delegate_function =
-    base::BindRepeating(&SyncEngineBackend::OnPollSyncCycle,
-                        weak_ptr_factory_.GetWeakPtr());
+      base::BindRepeating(&OnPollSyncCycle,
+                          sync_engine_impl,
+                          host);
 }
 
 }  // namespace syncer
+
+#include "../../../../../../components/sync/driver/glue/sync_engine_backend.cc"   // NOLINT
