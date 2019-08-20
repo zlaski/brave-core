d1e64b910914c3a0e5b445e6a393bfa173921326
Anton Lazarev <antonok35@gmail.com>
2019-08-20 16:20:52 -0700

    use cosmetic filtering api

--- components/brave_extension/extension/brave_extension/content.ts
+++ components/brave_extension/extension/brave_extension/content.ts
@@ -1,24 +1,89 @@
 const unique = require('unique-selector').default
 
 let target: EventTarget | null
+let genericExceptions: Array<string> | undefined = undefined
+let classIdBuffer: { classes: string[], ids: string[] } = { classes: [], ids: [] }
+
+const queriedIds = new Set()
+const queriedClasses = new Set()
+const regexWhitespace = /\s/
 
 function getCurrentURL () {
   return window.location.hostname
 }
 
-/*function applyCosmeticFilterMutationObserver (filterList: any) {
+const getClassesAndIds = function (addedNodes: Element[]) {
+  const ids = []
+  const classes = []
+
+  for (const node of addedNodes) {
+    let nodeId = node.id
+    if (nodeId && nodeId.length !== 0) {
+      nodeId = nodeId.trim()
+      if (!queriedIds.has(nodeId) && nodeId.length !== 0) {
+        ids.push(nodeId)
+        queriedIds.add(nodeId)
+      }
+    }
+    let nodeClass = node.className
+    if (nodeClass && nodeClass.length !== 0 && !regexWhitespace.test(nodeClass)) {
+      if (!queriedClasses.has(nodeClass)) {
+        classes.push(nodeClass)
+        queriedClasses.add(nodeClass)
+      }
+    } else {
+      let nodeClasses = node.classList
+      if (nodeClasses) {
+        let j = nodeClasses.length
+        while (j--) {
+          const nodeClassJ = nodeClasses[j]
+          if (queriedClasses.has(nodeClassJ) === false) {
+            classes.push(nodeClassJ)
+            queriedClasses.add(nodeClassJ)
+          }
+        }
+      }
+    }
+  }
+  return { classes, ids }
+}
+
+const handleNewNodes = (newNodes: Element[]) => {
+  const { classes, ids } = getClassesAndIds(newNodes)
+  if (!(genericExceptions)) {
+    classIdBuffer.classes.push(...classes)
+    classIdBuffer.ids.push(...ids)
+  } else {
+    chrome.runtime.sendMessage({
+      type: 'classIdStylesheet',
+      classes,
+      ids,
+      exceptions: genericExceptions
+    })
+  }
+}
+
+function applyCosmeticFilterMutationObserver () {
   let targetNode = document.documentElement
-  let observer = new MutationObserver(function (mutations) {
-    console.log('mutation observed')
-    injectIncrementalStyles(mutations)
+  let observer = new MutationObserver(mutations => {
+    const nodeList: Element[] = []
+    for (const mutation of mutations) {
+      for (let nodeIndex = 0; nodeIndex < mutation.addedNodes.length; nodeIndex++) {
+        nodeList.push(mutation.addedNodes[nodeIndex] as Element)
+      }
+    }
+    handleNewNodes(nodeList)
   })
   let observerConfig = {
     childList: true,
     subtree: true
-    // characterData: true
   }
   observer.observe(targetNode, observerConfig)
-}*/
+}
+
+let allNodes = Array.from(document.querySelectorAll('[id],[class]'))
+handleNewNodes(allNodes)
+applyCosmeticFilterMutationObserver()
 
 document.addEventListener('contextmenu', (event) => {
   // send host and store target
@@ -35,6 +100,22 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   switch (action) {
     case 'getTargetSelector': {
       sendResponse(unique(target))
+      break
+    }
+    case 'cosmeticFilterGenericExceptions': {
+      genericExceptions = msg.exceptions
+      if (classIdBuffer.classes.length !== 0 || classIdBuffer.ids.length !== 0) {
+        chrome.runtime.sendMessage({
+          type: 'classIdStylesheet',
+          classes: classIdBuffer.classes,
+          ids: classIdBuffer.ids,
+          exceptions: genericExceptions
+        })
+        classIdBuffer.classes = []
+        classIdBuffer.ids = []
+      }
+      sendResponse(null)
+      break
     }
   }
 })
