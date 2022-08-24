daea97e21420f42bce8690ae1ef539b1ee9e7f83
Arthur Edelstein <arthuredelstein@gmail.com>
2022-08-24 16:12:21 -0700

    performance navigation timing

---
+++ chromium_src/third_party/blink/renderer/core/timing/performance_navigation_timing.cc
@@ -0,0 +1,24 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "third_party/blink/renderer/core/timing/performance.h"
+#include "third_party/blink/renderer/core/timing/performance_entry.h"
+#include "third_party/blink/renderer/core/timing/performance_navigation_timing.h"
+#include "third_party/blink/renderer/core/timing/performance_resource_timing.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
+
+#define resource_timing_info_(...) \
+  resource_timing_info_(__VA_ARGS__),             \
+  allow_fingerprinting_(brave::AllowFingerprinting(window))
+
+#define MonotonicTimeToDOMHighResTimeStamp(...) \
+  RoundDOMHighResTimeStamp(allow_fingerprinting_,                       \
+    Performance::MonotonicTimeToDOMHighResTimeStamp(__VA_ARGS__))
+
+#include "src/third_party/blink/renderer/core/timing/performance_navigation_timing.cc"
+
+#undef MonotonicTimeToDOMHighResTimeStamp
+
+#undef resource_timing_info
---
+++ chromium_src/third_party/blink/renderer/core/timing/performance_navigation_timing.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_TIMING_PERFORMANCE_NAVIGATION_TIMING_H_
+
+#define BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_TIMING_PERFORMANCE_NAVIGATION_TIMING_H_
+
+#define resource_timing_info_ \
+  resource_timing_info_;      \
+  const bool allow_fingerprinting_
+
+#include "src/third_party/blink/renderer/core/timing/performance_navigation_timing.h"
+
+#undef resource_timing_info_
+
+#endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_TIMING_PERFORMANCE_NAVIGATION_TIMING_H_
