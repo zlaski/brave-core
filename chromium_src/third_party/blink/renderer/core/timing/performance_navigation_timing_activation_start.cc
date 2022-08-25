54d3be60800986178b8623192fc9f2e04c1e3f25
Arthur Edelstein <arthuredelstein@gmail.com>
2022-08-25 13:24:02 -0700

    performance_navigation_timing_activation_start rounding

---
+++ chromium_src/third_party/blink/renderer/core/timing/performance_navigation_timing_activation_start.cc
@@ -0,0 +1,15 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "third_party/blink/renderer/core/timing/performance.h"
+
+#define MonotonicTimeToDOMHighResTimeStamp(...) \
+  RoundDOMHighResTimeStamp(                     \
+       performance_navigation_timing.GetExecutionContext(), \
+       Performance::MonotonicTimeToDOMHighResTimeStamp(__VA_ARGS__))
+
+#include "src/third_party/blink/renderer/core/timing/performance_navigation_timing_activation_start.cc"
+
+#undef MonotonicTimeToDOMHighResTimeStamp
