441ea8e58f9cc83572d576ae56a45924fdcd321f
tapanmodh <modhtapan@gmail.com>
2023-05-08 15:25:26 +0530

    wip

--- components/language/android/java/src/org/chromium/components/language/BraveLocaleManagerDelegateImpl.java
+++ components/language/android/java/src/org/chromium/components/language/BraveLocaleManagerDelegateImpl.java
@@ -4,31 +4,39 @@
 
 package org.chromium.components.language;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import org.chromium.base.ContextUtils;
 import android.app.LocaleManager;
 import android.os.LocaleList;
 
-public class BraveLocaleManagerDelegateImpl extends LocaleManagerDelegateImpl {
+import org.chromium.base.ContextUtils;
+import org.chromium.base.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class BraveLocaleManagerDelegateImpl extends LocaleManagerDelegateImpl {
     @Override
     public Locale getApplicationLocale() {
+
+        Log.e("tapan", "BraveLocaleManagerDelegateImpl getApplicationLocale");
         // TODO(https://crbug.com/1293523): Replace with calls to {@link LocaleManager} once the T
         // SDK is available.
-        LocaleList currentAppLocales = ContextUtils.getApplicationContext().getSystemService(LocaleManager.class).getApplicationLocales();
-        if(!currentAppLocales.isEmpty()) {
+        LocaleList currentAppLocales = ContextUtils.getApplicationContext()
+                                               .getSystemService(LocaleManager.class)
+                                               .getApplicationLocales();
+        if (!currentAppLocales.isEmpty()) {
             return currentAppLocales.get(0);
         }
         return null;
-    }   
+    }
 
     @Override
     public void setApplicationLocale(String languageName) {
         // TODO(https://crbug.com/1293523): Replace with calls to {@link LocaleManager} once the T
         // SDK is available.
-        ContextUtils.getApplicationContext().getSystemService(LocaleManager.class
-    ).setApplicationLocales(new LocaleList(Locale.forLanguageTag(languageName)));
+        Log.e("tapan", "BraveLocaleManagerDelegateImpl setApplicationLocale:"+languageName);
+        /*ContextUtils.getApplicationContext()
+                .getSystemService(LocaleManager.class)
+                .setApplicationLocales(new LocaleList(Locale.forLanguageTag(languageName)));*/
     }
 }
