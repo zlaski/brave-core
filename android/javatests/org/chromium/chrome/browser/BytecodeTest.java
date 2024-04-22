53bd7fcfe119577dac168ff8cb1ee6f18c62f660
Artem Samoilenko <artem@brave.com>
2024-04-22 17:11:18 -0400

    [Android] Site settings cleanup
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/cd571cca55eb76e6a731cecc9b17960541627141
    
    Reland "[Clank] Clank site settings cleanup"
    
    This reverts commit c4eaf6716dee77ed4b411f175b361d72a44a34c2.
    
    Reason for revert: This CL was not causing failures, see b/333905073
    
    Original change's description:
    > Revert "[Clank] Clank site settings cleanup"
    >
    > This reverts commit 65cc6e933535beb1cfde7e189db9bb601677be36.
    >
    > Reason for revert: Suspect causing test failures on OmniboxPedalsTest. See b/333905073.
    >
    > Original change's description:
    > > [Clank] Clank site settings cleanup
    > >
    > > Cleaning up some obsolete params in site settings.
    > >
    > > Bug: 254415177
    > > Change-Id: I2d173057985032940190bf7c748b9019a08eaca8
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5447008
    > > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > > Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1285856}
    >
    > Bug: 254415177
    > Change-Id: Ib3b5d5656f0226b15c1e599b1bfcde6c9ba2c9c2
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5447091
    > Auto-Submit: Hailey Wang <haileywang@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Hailey Wang <haileywang@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1285972}
    
    Bug: 254415177

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -122,7 +122,6 @@ import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.browser_ui.site_settings.ContentSettingException;
 import org.chromium.components.browser_ui.site_settings.PermissionInfo;
 import org.chromium.components.browser_ui.site_settings.SiteSettingsCategory;
-import org.chromium.components.browser_ui.site_settings.SiteSettingsDelegate;
 import org.chromium.components.browser_ui.site_settings.Website;
 import org.chromium.components.browser_ui.site_settings.WebsiteAddress;
 import org.chromium.components.browser_ui.widget.MenuOrKeyboardActionController;
@@ -721,8 +720,7 @@ public class BytecodeTest {
                         true,
                         getClassForPath(
                                 "org/chromium/components/browser_ui/site_settings/ContentSettingsResources$ResourceItem"),
-                        int.class,
-                        SiteSettingsDelegate.class));
+                        int.class));
         Assert.assertTrue(
                 methodExists(
                         "org/chromium/components/browser_ui/site_settings/ContentSettingsResources",
@@ -730,8 +728,7 @@ public class BytecodeTest {
                         true,
                         getClassForPath(
                                 "org/chromium/components/browser_ui/site_settings/ContentSettingsResources$ResourceItem"),
-                        int.class,
-                        SiteSettingsDelegate.class));
+                        int.class));
         Assert.assertTrue(methodExists(
                 "org/chromium/components/browser_ui/site_settings/SingleCategorySettings",
                 "getAddExceptionDialogMessage", true, String.class));
