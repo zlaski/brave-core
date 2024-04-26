a171614831edaef60b6da78cfd8694c043e099d6
Artem Samoilenko <artem@brave.com>
2024-04-26 10:55:10 -0400

    [Android] New param for MultiInstanceManagerApi31 class
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/94794635ed8558f2892ff4055c93239492448e68
    
    [TabTearing] Restrict closing empty window after tab reparenting to
    desktop windowing mode
    
    Currently, when the only tab in a Chrome window is dragged out and into
    another Chrome window in multiwindow mode, the empty source window is
    closed. Restrict closing the empty window to desktop windowing mode,
    such a window should continue to exist in split screen after tab
    reparenting.
    
    Bug: 333411285

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1126,7 +1126,8 @@ public class BytecodeTest {
                         MultiWindowModeStateDispatcher.class,
                         ActivityLifecycleDispatcher.class,
                         ObservableSupplier.class,
-                        MenuOrKeyboardActionController.class));
+                        MenuOrKeyboardActionController.class,
+                        ObservableSupplier.class));
         Assert.assertTrue(
                 constructorsMatch(
                         "org/chromium/chrome/browser/multiwindow/MultiWindowUtils",
