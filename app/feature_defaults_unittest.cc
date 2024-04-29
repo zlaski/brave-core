9d2ee818a95457310ad33dd5002f054f847a72f4
Claudio DeSouza <cdesouza@brave.com>
2024-04-29 22:36:28 +0100

    [WIP] `kLegacyTechReportEnableCookieIssueReports` deleted
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/6c52ec56cbc9fc2e5a3d4bf8ac0bc2ba07415943
    
    commit 6c52ec56cbc9fc2e5a3d4bf8ac0bc2ba07415943
    Author: Sandor Major <sandormajor@google.com>
    Date:   Thu Apr 18 20:04:03 2024 +0000
    
        Delete LegacyTechReportEnableCookieIssueReports flag and related code
    
        Bug: b/324847121, 330367496

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -152,7 +152,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &features::kIdleDetection,
       &features::kKAnonymityService,
       &features::kKAnonymityServiceOHTTPRequests,
-      &features::kLegacyTechReportEnableCookieIssueReports,
       &features::kNewTabPageTriggerForPrerender2,
       &features::kNotificationTriggers,
       &features::kOmniboxTriggerForNoStatePrefetch,
