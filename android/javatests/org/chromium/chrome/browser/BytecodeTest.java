8ee11af2c45617bbc5e89c26b191d6937aa1aab7
AlexeyBarabash <alexey@brave.com>
2024-05-10 18:59:06 +0300

    Changed parameter at MultiInstanceManagerApi31.ctor
    Related Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/d294821b0548c2021e27ea625f9f4230d556ffbf
    
    	[TSLO] Update MultiInstanceManagerApi31 to use
    	DesktopWindowStateProvider
    
    	This CL removes the boolean desktop window mode supplier used by
    	MultiInstanceManager and injects a DesktopWindowStateProvider supplier
    	instead, since the latter is instantiated after MultiInstanceManager
    	on startup.
    
    	Bug: 325351108
    	Change-Id: Ibe3c7cbb66cc0a31459d705e86ca69313d99b63a
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5503037

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1195,7 +1195,7 @@ public class BytecodeTest {
                         ActivityLifecycleDispatcher.class,
                         ObservableSupplier.class,
                         MenuOrKeyboardActionController.class,
-                        ObservableSupplier.class));
+                        Supplier.class));
         Assert.assertTrue(
                 constructorsMatch(
                         "org/chromium/chrome/browser/multiwindow/MultiWindowUtils",
