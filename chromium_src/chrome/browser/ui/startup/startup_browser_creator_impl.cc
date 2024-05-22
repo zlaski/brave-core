9365b288d6901aaf7a8237e305e29bb3af87c617
Emerick Rogul <erogul@brave.com>
2024-05-21 22:28:39 -0400

    fixup! `kForYouFre` feature flag removed; keep first run experience disabled

--- chromium_src/chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chromium_src/chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -3,9 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "brave/browser/ui/startup/brave_startup_tab_provider_impl.h"
 #include "build/build_config.h"
 #include "chrome/browser/buildflags.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/startup/startup_browser_creator.h"
+#include "chrome/browser/ui/startup/startup_tab_provider.h"
 #include "components/signin/public/base/signin_buildflags.h"
 
 #if BUILDFLAG(ENABLE_UPDATER)
@@ -32,8 +35,12 @@ void MaybeShowPromotionInfoBar(
   BrowserList::GetInstance()->GetLastActive
 #endif
 
+#define StartupTabProviderImpl BraveStartupTabProviderImpl
+
 #include "src/chrome/browser/ui/startup/startup_browser_creator_impl.cc"
 
 #if BUILDFLAG(IS_MAC) && BUILDFLAG(ENABLE_UPDATER)
 #undef GetLastActive
 #endif
+
+#undef StartupTabProviderImpl
