b001fb15d114ff0c36103d3734a74b52ce99ea37
Claudio DeSouza <cdesouza@brave.com>
2024-06-21 13:38:29 +0100

    `GetChromeAppsFolder` moved into its own file
    This change in upstream requires our updating our overrides for this
    function, which includes changing patches and overlay files.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/968c15844c55c0ae6f44f2abc87e73bf8b8a3716
    
    commit 968c15844c55c0ae6f44f2abc87e73bf8b8a3716
    Author: Marijn Kruisselbrink <mek@chromium.org>
    Date:   Thu Jun 20 17:58:16 2024 +0000
    
        [PWAs] Split GetChromeAppsFolder() into its own file.
    
        This is part 6/x of a series of CLs to clean up/organize the mac
        PWA OS integration code. Even though this helper method is
        only used in web_app_shortcut_mac.mm, splitting them of into their
        own file still makes it easier to navigate the mac web app shortcut
        code, as well as aid with further cleanups where various callers
        of this method might end up in separate files.
    
        This also moved the logic for setting the localized name and icon
        of the folder into GetChromeAppsFolder() itself. As this is the
        method that actually creates the folder it makes sense for it to
        also make sure it has the correct name and icon.
    
        In follow up CLs more code will move to the os_integration/mac
        directory, ultimately making that its own build target as well
        (although for now files are kept in their original build files).
    
        Bug: 347167972

--- chromium_src/chrome/browser/web_applications/os_integration/mac/apps_folder_support.mm
+++ chromium_src/chrome/browser/web_applications/os_integration/mac/apps_folder_support.mm
@@ -1,7 +1,7 @@
-/* Copyright (c) 2019 The Brave Authors. All rights reserved.
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ /* Copyright (c) 2019 The Brave Authors. All rights reserved.
+  * This Source Code Form is subject to the terms of the Mozilla Public
+  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 namespace base {
 class FilePath;
@@ -11,7 +11,7 @@ namespace {
 base::FilePath GetLocalizableBraveAppShortcutsSubdirName();
 }
 
-#include "src/chrome/browser/web_applications/os_integration/web_app_shortcut_mac.mm"
+#include "src/chrome/browser/web_applications/os_integration/mac/apps_folder_support.mm"
 
 namespace {
 base::FilePath GetLocalizableBraveAppShortcutsSubdirName() {
