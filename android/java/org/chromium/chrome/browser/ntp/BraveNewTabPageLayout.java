6401f6c6bbfa45a5670712bde022c7bc25787ee0
Artem Samoilenko <artem@brave.com>
2024-08-02 12:46:52 -0400

    [Android] Instantiate SettingsLauncherImpl through Factory
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/524abae6e1c36e90900f20b1eb4aeb784582df6a
    
    [Android] Instantiate SettingsLauncherImpl through Factory
    
    This is a simple replace for easy review, will go through usages that
    can be simplified in a follow-up.
    
    Bug: 354019554

--- android/java/org/chromium/chrome/browser/ntp/BraveNewTabPageLayout.java
+++ android/java/org/chromium/chrome/browser/ntp/BraveNewTabPageLayout.java
@@ -96,7 +96,7 @@ import org.chromium.chrome.browser.profiles.ProfileManager;
 import org.chromium.chrome.browser.rate.RateUtils;
 import org.chromium.chrome.browser.settings.BackgroundImagesPreferences;
 import org.chromium.chrome.browser.settings.BraveNewsPreferencesV2;
-import org.chromium.chrome.browser.settings.SettingsLauncherImpl;
+import org.chromium.chrome.browser.settings.SettingsLauncherFactory;
 import org.chromium.chrome.browser.suggestions.tile.BraveMostVisitedTilesLayoutBase;
 import org.chromium.chrome.browser.suggestions.tile.TileGroup.Delegate;
 import org.chromium.chrome.browser.tab.Tab;
@@ -346,10 +346,13 @@ public class BraveNewTabPageLayout
         mNewContentProgressBar = findViewById(R.id.new_content_loading_spinner);
         mNewContentLayout.setOnClickListener(view -> { loadNewContent(); });
         ImageView ivNewsSettings = findViewById(R.id.news_settings_button);
-        ivNewsSettings.setOnClickListener(view -> {
-            SettingsLauncher settingsLauncher = new SettingsLauncherImpl();
-            settingsLauncher.launchSettingsActivity(getContext(), BraveNewsPreferencesV2.class);
-        });
+        ivNewsSettings.setOnClickListener(
+                view -> {
+                    SettingsLauncher settingsLauncher =
+                            SettingsLauncherFactory.createSettingsLauncher();
+                    settingsLauncher.launchSettingsActivity(
+                            getContext(), BraveNewsPreferencesV2.class);
+                });
 
         mRecyclerView = findViewById(R.id.recyclerview);
         LinearLayoutManagerWrapper linearLayoutManager =
