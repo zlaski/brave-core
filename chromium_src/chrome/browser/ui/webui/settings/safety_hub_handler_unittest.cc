e49578aca6c3fb08372293f9e73aab11973aac3f
Claudio DeSouza <cdesouza@brave.com>
2024-08-24 12:36:58 +0100

    Fixing substitution in `safety_hub_handler_unittest.cc`
    This file started pulling in some other headers, which eventually run
    into unwanted substitution for the toke `MIDI`.

--- chromium_src/chrome/browser/ui/webui/settings/safety_hub_handler_unittest.cc
+++ chromium_src/chrome/browser/ui/webui/settings/safety_hub_handler_unittest.cc
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include "chrome/browser/ui/safety_hub/safety_hub_test_util.h"
 #include "components/content_settings/core/common/content_settings_types.h"
 
 // clang-format off
